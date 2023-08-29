0cd4c99c64bb7af0041020a5d697366dfec8a766
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-29 06:35:26 +0200

    tenzie game

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
-const App = () => {};
+import Die from './component/Die.jsx';
+
+const App = () => {
+  const allNewDice = () => {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.ceil(Math.random() * 6);
+      newDice.push(randomNumber);
+    }
+    return newDice;
+  };
+  allNewDice();
+
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+      </div>
+    </main>
+  );
+};
 
 export default App;
---
+++ src/component/Die.jsx
@@ -0,0 +1,8 @@
+const Die = (props) => {
+  return (
+    <div className="die-container">
+      <h2>{props.value} </h2>
+    </div>
+  );
+};
+export default Die;
